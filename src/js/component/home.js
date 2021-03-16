86ad93fa4514dee03f927881a106b913b46c87d4
alonsog3a <alonsog3a@gmail.com>
2021-03-16 22:59:26 +0000

    Archivo para revision

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+//import "./styles.css";
+//import { Ligthgreen } from "./styles.js";
+//import { Ligthyellow } from "./styles.js";
+//import { Ligthred } from "./styles.js";
+//import { Trafficligth } from "./styles.js";
+//import { Ligth1 } from "./styles.js";
+//import { Card } from "./styles.js";
+//import { Button1 } from "./styles.js";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [color, setcolor] = useState(null);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card">
+			<div className="trafficligth">
+				<div className="ligth1">
+					<div
+						className={
+							"light red " +
+							(color == "light red" ? "light shadows" : null)
+						}
+						onClick={() => setcolor("light red")}></div>
+				</div>
+
+				<div className="ligth2">
+					<div
+						className={
+							"light yellow " +
+							(color == "light yellow" ? "light shadows" : null)
+						}
+						onClick={() => setcolor("light yellow")}></div>
+				</div>
+				<div className="ligth3">
+					<div
+						className={
+							"light green " +
+							(color == "light green" ? "light shadows" : null)
+						}
+						onClick={() => setcolor("light green")}></div>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/js/component/styles.js
@@ -0,0 +1,52 @@
+//import React, { useState } from "react";
+
+//export const Ligthred = {
+//write button styles here
+
+//backgroundColor: "red",
+//width: "50px",
+//height: "50px",
+//borderRadius: "50px"
+//};
+//export const Ligthyellow = {
+//write button styles here
+
+//backgroundColor: "yellow",
+//width: "50px",
+//height: "50px",
+//borderRadius: "50px"
+//};
+
+//export const Ligthgreen = {
+//write button styles here
+
+//backgroundColor: "green",
+//width: "50px",
+//height: "50px",
+//borderRadius: "50px"
+//};
+
+//export const Trafficligth = {
+//backgroundColor: "black",
+//width: "80px",
+//height: "200px",
+//borderRadius: "20px",
+//justifyContent: "center",
+//alignItems: "center",
+//paddingTop: "8px"
+//};
+
+//export const Ligth1 = {
+//padding: "5px",
+//justifyContent: " center",
+//alignItems: "center",
+//display: "flex"
+//};
+
+//export const Card = {
+//padding: "20px",
+//justifyContent: "center",
+//alignItems: "center"
+//};
+
+//export const Button1 = <button style={Ligthgreen}></button>;
